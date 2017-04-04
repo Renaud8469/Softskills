31fffbb49af9b3667381d6861bac45d36a8a86dd
Renaud Dahl <renaud.dahl@student.ecp.fr>
2017-04-04 14:22:42 +0200

    Remove useless file

--- text_analysis.py
+++
@@ -1,4 +0,0 @@
-import requests
-
-angry_text = "Hey Julian, come over here ! I got a couple things to tell you. Where the hell do you think you are ? I heard you're always late. We already talked about this. Do you think this is a hotel or something ? Let me tell you, you're not that far from being sacked, boy."
-
